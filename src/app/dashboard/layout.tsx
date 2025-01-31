24259a8008b4d2b7fd25b595b0b5004e97945d28
Yusha25sd <ayushkumarmehta3@gmail.com>
2025-01-31 08:22:32 +0530

    dynamic data fix

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,14 +1,16 @@
 import Navbar from '@/components/NavbarDashboard';
-
+import { Suspense } from 'react';
 interface RootLayoutProps {
   children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
+    <Suspense fallback={<div>Loading...</div>}>
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <main className="flex-grow">{children}</main>
     </div>
+    </Suspense>
   );
 }
